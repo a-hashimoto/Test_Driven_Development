3fe34ce94f5e6a2eec42b9a49c1f27c96da1daab
a-hashimoto <a-hashimoto@unext.jp>
2017-10-26 17:40:24 +0900

    ToDoListをこなす

---
+++ a-hashimoto/app/src/main/java/com/example/a_hashimoto/testdrivendevelopment/Dollar.java
@@ -0,0 +1,15 @@
+package com.example.a_hashimoto.testdrivendevelopment;
+
+/**
+ * Created by a-hashimoto on 2017/10/26.
+ */
+
+class Dollar {
+    public int amount;
+    public Dollar(int amount){
+
+    }
+    public void times(int multiplier){
+
+    }
+}
